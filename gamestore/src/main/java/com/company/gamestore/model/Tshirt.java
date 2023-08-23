adfaa6c99ddd9be480f7101f6458003b5b5d8cc3
liannnaa <lnp2016@nyu.edu>
2023-08-22 23:39:46 -0500

    added tshirt model

---
+++ gamestore/src/main/java/com/company/gamestore/model/Tshirt.java
@@ -0,0 +1,78 @@
+package com.company.gamestore.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="tshirt")
+public class Tshirt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int tshirtId;
+    private String size;
+    private String color;
+    private String description;
+    private Double price;
+    private int quantity;
+
+    public int getTshirtId() {
+        return tshirtId;
+    }
+
+    public void setTshirtId(int tshirtId) {
+        this.tshirtId = tshirtId;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tshirt tshirt = (Tshirt) o;
+        return tshirtId == tshirt.tshirtId && quantity == tshirt.quantity && Objects.equals(size, tshirt.size) && Objects.equals(color, tshirt.color) && Objects.equals(description, tshirt.description) && Objects.equals(price, tshirt.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tshirtId, size, color, description, price, quantity);
+    }
+}
